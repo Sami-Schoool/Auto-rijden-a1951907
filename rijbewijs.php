f644396b35b1299bfe4c579cf571530c16986107
Sami School <2077560@talnet.nl>
2020-05-28 11:33:01 +0200

    Uitwerking van de exercise

---
+++ rijbewijs.php
@@ -0,0 +1,20 @@
+<?php
+
+    
+    $leeftijd = readline("wat is je leeftijd? \n");
+    $min_leeftijd =18; 
+
+        if($leeftijd >= $min_leeftijd)
+        { 
+        
+            echo "Je mag beginnen met rijlessen!";
+        
+        }
+
+    else { 
+    
+        echo "Helaas, je mag nog niet beginnen met rijlessen";
+        
+    }
+
+?>
